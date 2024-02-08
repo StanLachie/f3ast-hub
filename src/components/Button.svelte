a3a393b135a007aec1afa17f655f1b5bcb9b81b1
Lachie <lachiejeffress@gmail.com>
2024-02-08 19:10:38 +1000

    Add Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	export let variant: 'outline' | 'primary' = 'primary';
+	export let to: string | undefined = undefined;
+	export let onClick: () => void = () => {};
+
+	const buttonBase =
+		'py-2 px-3 font-semibold shadow-sm text-sm rounded-lg hover:scale-[97.5%] transition-all';
+
+	const variantStyles = {
+		primary: twMerge(buttonBase, 'bg-emerald-300 text-emerald-950 hover:bg-emerald-400 border-emerald-300 border'),
+		outline: twMerge(buttonBase, 'hover:bg-neutral-100 border')
+	};
+</script>
+
+<a href={to}>
+	<button on:click={onClick} class={twMerge(`${variantStyles[variant]}`, `${$$restProps.class}`)}>
+		<slot />
+	</button>
+</a>
