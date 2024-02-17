4845b3b3cb4f4fca02ff58fbcc4997ef178711b1
Lachie <lachiejeffress@gmail.com>
2024-02-17 22:45:58 +1000

    Update LearnMore component

--- src/components/blocks/LearnMore.svelte
+++ src/components/blocks/LearnMore.svelte
@@ -4,54 +4,6 @@
 			So, what exactly is F3AST, and how will it help my business?
 		</h1>
 	</div>
-
-	<div class="max-w-6xl columns-1 gap-x-6 md:columns-3">
-		<div class="mb-4 rounded-lg border-2 border-emerald-300 p-3 font-semibold">
-			<h2 class="mb-1 text-center text-xl text-emerald-400">Get an Affordable Online Presence</h2>
-			<p class="text-pretty text-center">
-				Embrace the online world with F3AST. Starting at $19.99 a month, we offer multiple plans to
-				cater to businesses of any size.
-			</p>
-		</div>
-		<div class="mb-4 rounded-lg border-2 border-emerald-300 p-3 font-semibold">
-			<h2 class="mb-1 text-center text-xl text-emerald-400">Connect Directly with Customers</h2>
-			<p class="text-pretty text-center">
-				F3AST&apos;s integrated inquiry system allows you to connect with your customers directly
-				via. email, without any hassle. We&apos;ll handle the tech, so you can focus on what matters
-				most: your business.
-			</p>
-		</div>
-		<div class="mb-4 rounded-lg border-2 border-emerald-300 p-3 font-semibold">
-			<h2 class="mb-1 text-center text-xl text-emerald-400">Customize Your Website</h2>
-			<p class="text-pretty text-center">
-				Your business is unique, and your online presence should be too. With F3AST, you&apos;ll be
-				able to chose your websites URL, with a F3AST vanity URL. <br />(e.g.
-				www.[your-business].f3ast.com)
-			</p>
-		</div>
-		<div class="mb-4 rounded-lg border-2 border-emerald-300 p-3 font-semibold">
-			<h2 class="mb-1 text-center text-xl text-emerald-400">Boost Your Search Ranking</h2>
-			<p class="text-pretty text-center">
-				F3AST&apos;s SEO tools help you rank higher in search results, so more potential customers
-				can find your business online.
-			</p>
-		</div>
-		<div class="mb-4 rounded-lg border-2 border-emerald-300 p-3 font-semibold">
-			<h2 class="mb-1 text-center text-xl text-emerald-400">Enjoy Stress-Free Tech Support</h2>
-			<p class="text-pretty text-center">
-				Leave the tech troubles to us. From seamless web design to ongoing maintenance, our team at
-				F3AST will ensure your digital journey is smooth sailing.
-			</p>
-		</div>
-		<div class="mb-4 rounded-lg border-2 border-emerald-300 p-3 font-semibold">
-			<h2 class="mb-1 text-center text-xl text-emerald-400">Make It Easy for Customers</h2>
-			<p class="text-pretty text-center">
-				Open your doors online 24/7. Let customers explore your menu, dive into your story, and
-				connect with you effortlessly, with just a few clicks.
-			</p>
-		</div>
-	</div>
-
 	<div class="max-w-4xl text-lg">
 		<ul class="list-inside list-disc">
 			<h2 class="mt-4 text-2xl font-semibold text-emerald-400">
