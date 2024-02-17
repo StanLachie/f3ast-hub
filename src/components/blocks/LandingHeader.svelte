4c999bec9e965d58a6ea98510ea6d1dea2537891
Lachie <lachiejeffress@gmail.com>
2024-02-17 20:22:13 +1000

    Add LandingHeader component

---
+++ src/components/blocks/LandingHeader.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Button from '../Button.svelte';
+</script>
+
+<div class="flex w-full justify-between px-16 py-6">
+	<h1 class="font-norwester text-4xl">F3AST</h1>
+	<div class="flex gap-4">
+		<Button variant="outline">Dashboard</Button>
+		<Button>Contact Us</Button>
+	</div>
+</div>
