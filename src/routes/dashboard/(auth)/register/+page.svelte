605a7d2b23d58dcbd38c30ccc44de41933cd007b
Lachie <lachiejeffress@gmail.com>
2024-03-14 17:27:12 +1000

    .

--- src/routes/dashboard/(auth)/register/+page.svelte
+++ src/routes/dashboard/(auth)/register/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { PageData } from '../register/$types';
 
 	export let data: PageData;
 </script>
 
 <form method="POST">
-	<h1 class="text-2xl font-semibold">Signup</h1>
+	<h1 class="text-2xl font-semibold">Register</h1>
 	<input type="text" name="First Name" placeholder="First Name" />
 	<input type="text" name="Last Name" placeholder="Last Name" />
 	<input type="email" name="Email" placeholder="Email" />
 	<input type="text" name="Username" placeholder="Username" />
 	<input type="password" name="Password" placeholder="Password" />
 	<input type="password" name="Confirm Password" placeholder="Confirm Passoword" />
-	<button class="btn-primary w-full"> Signup </button>
+	<button class="btn-primary w-full"> Register </button>
 </form>
