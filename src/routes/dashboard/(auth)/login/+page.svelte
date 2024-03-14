d0297986b253c613da56b4e8c1d09859ed2a9643
Lachie <lachiejeffress@gmail.com>
2024-03-14 17:34:15 +1000

    .

--- src/routes/dashboard/(auth)/login/+page.svelte
+++ src/routes/dashboard/(auth)/login/+page.svelte
@@ -9,9 +9,8 @@
 	<input type="text" name="username" placeholder="Username" />
 	<input type="password" name="password" placeholder="Password" />
 	<button class="btn-primary w-full"> Login </button>
-	<span class="">
+	<span class=" text-neutral-600">
 		Need an account?
-		<a class=" underline" href="/dashboard/register"> register </a>
+		<a class=" underline text-emerald-500" href="/dashboard/register"> register </a>
 	</span>
-	<!-- Add a styling tag to the a element add an underline, text emerald 300. finally add a styling tag to the span: text-neutral-500 -->
 </form>
