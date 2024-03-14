4dbd3ea88d3bab6ee97de9eac6d7a3b4af2849c9
Lachie <lachiejeffress@gmail.com>
2024-03-14 18:38:41 +1000

    .

--- src/routes/dashboard/(dash)/+layout.svelte
+++ src/routes/dashboard/(dash)/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import { page } from '$app/stores';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
@@ -23,17 +24,19 @@
 			link: '/dashboard/logout'
 		}
 	];
+
+	$: currentRoute = $page.url.pathname;
 </script>
 
 <div class="flex min-h-screen w-screen divide-x divide-neutral-400">
 	<div class="flex h-screen w-16 flex-col justify-between bg-white">
-		<div class="flex flex-col divide-y divide-neutral-400 border-b border-neutral-400">
+		<div class="group flex flex-col divide-y divide-neutral-400 border-b border-neutral-400">
 			{#each sidebarItems as item}
 				<a
 					href={item.link}
 					class="flex h-16 items-center justify-center text-2xl hover:bg-neutral-100"
 				>
-					<Icon icon={item.icon} />
+					<Icon icon={item.icon} class={currentRoute === item.link ? 'text-emerald-300' : ''} />
 				</a>
 			{/each}
 		</div>
@@ -43,7 +46,7 @@
 					href={item.link}
 					class="flex h-16 items-center justify-center text-2xl hover:bg-neutral-100"
 				>
-					<Icon icon={item.icon} />
+					<Icon icon={item.icon} class={currentRoute === item.link ? 'text-emerald-300' : ''} />
 				</a>
 			{/each}
 		</div>
