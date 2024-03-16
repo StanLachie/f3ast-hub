f3a7751bafbfc08629ac7415034d28801871e3c1
Lachie <lachiejeffress@gmail.com>
2024-03-16 12:13:27 +1000

    .

--- src/routes/r/[slug]/menu/+page.svelte
+++ src/routes/r/[slug]/menu/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Hero from '../../../$lib/components/blocks/restaurant/Hero.svelte';
+	import Hero from '$lib/components/blocks/restaurant/Hero.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
