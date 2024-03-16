ead1a22c6f398138e93960fe5d33b944aa35395e
Lachie <lachiejeffress@gmail.com>
2024-03-16 12:18:20 +1000

    .

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,11 +1,14 @@
+
+
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
-	if (locals.session) {
+	const session = await locals.getSession();
+
+	if (session) {
 		redirect(302, '/dashboard/welcome');
 	} else {
 		redirect(302, '/dashboard/login');
-
 	}
 }) satisfies PageServerLoad;
