bdc2efd8ddfd6053f40cb1071856eac6acf4d35f
Lachie <lachiejeffress@gmail.com>
2024-03-16 16:57:35 +1000

    .

---
+++ src/routes/[restaurantSlug]/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
+<div class="h-dvh w-screen">
+	<div class="mx-auto h-1/2 max-w-4xl p-2 sm:p-8 md:p-16">
+		<slot />
+	</div>
+</div>
--- src/routes/[restaurantSlug]/+page.svelte
+++ src/routes/[restaurantSlug]/+page.svelte
@@ -1,9 +1,46 @@
 <script lang="ts">
 	import Hero from '$lib/components/blocks/restaurant/Hero.svelte';
 	import type { PageData } from './$types';
+	import Icon from '@iconify/svelte';
 
 	export let data: PageData;
 </script>
 
-<Hero />
-<h1>{data.restaurant.name}</h1>
+<meta:head>
+	<title>{data.restaurant.name}</title>
+</meta:head>
+
+<div class="relative">
+	<img
+		src={data.restaurant.banner}
+		alt={`${data.restaurant.name} Banner`}
+		class="h-44 w-full rounded-lg object-cover sm:h-48 md:h-60"
+	/>
+	<div
+		class="absolute inset-0 rounded-lg bg-gradient-to-t from-gray-900 via-transparent to-transparent"
+	></div>
+	<img
+		src={data.restaurant.logo}
+		alt={`${data.restaurant.name} Logo`}
+		class="absolute bottom-4 left-4 h-20 w-20 rounded-full border-4 border-white bg-white object-cover sm:h-24 sm:w-24"
+	/>
+</div>
+<div class="mt-2 flex flex-col">
+	<h1 class="text-2xl font-semibold">
+		{data.restaurant.name}
+	</h1>
+	<div class="group flex items-center gap-1 text-neutral-500">
+		<Icon icon="ion:location" class="h-4 w-4 group-hover:text-neutral-600" />
+		<a
+			href={`https://www.google.com/maps/search/?api=1&query=${data.restaurant.address}`}
+			target="_blank"
+			rel="noopener noreferrer"
+			class=" font-semibold group-hover:text-gray-700"
+		>
+			{data.restaurant.address}
+		</a>
+	</div>
+	<div class="text-pr mt-2 max-w-xl text-sm text-neutral-500">
+		{data.restaurant.bio}
+	</div>
+</div>
