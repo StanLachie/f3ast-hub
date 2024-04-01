8d9bf754f0aced12471b45d2539f49eb2d335ea2
Lachie <lachiejeffress@gmail.com>
2024-04-01 17:16:33 +1000

    .

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,7 @@
+export const config = {
+	runtime: 'edge'
+};
+
 export const load = async (event) => {
 	const session = await event.locals.getSession();
 	return {
--- src/routes/dashboard/(auth)/login/+page.svelte
+++ src/routes/dashboard/(auth)/login/+page.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
 
-	export let data: PageData;
+	let isSubmitting = false;
 </script>
 
-<form method="POST" class="form">
+<form
+	method="POST"
+	class="form"
+	use:enhance={() => {
+		isSubmitting = true;
+		return async ({ update }) => {
+			update({ invalidateAll: false }).finally(() => {
+				isSubmitting = false;
+			});
+		};
+	}}
+>
 	<h1 class="text-2xl font-semibold">Login</h1>
 	<input class="input" type="email" name="email" placeholder="Email" />
 	<input class="input" type="password" name="password" placeholder="Password" />
-	<button class="btn-primary w-full"> Login </button>
+	<button class="btn-primary w-full" disabled={isSubmitting} type="submit">
+		{isSubmitting ? 'Logging in...' : 'Login'}
+	</button>
 	<span class=" text-neutral-600">
 		Need an account?
 		<a class=" text-emerald-500 underline" href="/dashboard/register"> register </a>
