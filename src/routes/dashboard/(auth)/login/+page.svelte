c601cc00b0dc75e60a649531b28d163c280861d2
Lachie <lachiejeffress@gmail.com>
2024-04-18 20:48:38 +1000

    .

--- src/routes/dashboard/(auth)/login/+page.svelte
+++ src/routes/dashboard/(auth)/login/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
 
+	let error: string | null = null;
 	let isSubmitting = false;
 </script>
 
@@ -10,14 +12,31 @@
 	class="form"
 	use:enhance={() => {
 		isSubmitting = true;
-		return async ({ update }) => {
-			update({ invalidateAll: false }).finally(() => {
+		return async ({ result }) => {
+			console.log(result);
+			if (
+				result.type === 'failure' &&
+				result.data &&
+				result.data.error &&
+				result.data.error &&
+				typeof result.data.message === 'string'
+			) {
+				error = result.data.message;
 				isSubmitting = false;
-			});
+			} else {
+				goto('/dashboard');
+			}
 		};
 	}}
 >
 	<h1 class="text-2xl font-semibold">Login</h1>
+	{#if error}
+		<div
+			class="w-full rounded-lg border border-neutral-800 bg-red-400 px-3 py-2 text-center font-normal text-sm"
+		>
+			{error}
+		</div>
+	{/if}
 	<input class="input" type="email" name="email" placeholder="Email" />
 	<input class="input" type="password" name="password" placeholder="Password" />
 	<button class="btn-primary w-full" disabled={isSubmitting} type="submit">
