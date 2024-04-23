8ad3750a398c35433579bdc1fe4dfb12950f3613
StanLachie <lachiejeffress@gmail.com>
2024-04-23 16:05:10 +1000

    .

--- src/lib/components/blocks/LandingHeader.svelte
+++ src/lib/components/blocks/LandingHeader.svelte
@@ -3,7 +3,7 @@
 	import Button from '../Button.svelte';
 </script>
 
-<div class="flex w-full justify-between px-16 py-6">
+<div class="flex w-full justify-between px-4 py-6 sm:px-16">
 	<h1 class="font-norwester text-4xl">F3AST</h1>
 	<div class="flex gap-4">
 		<a class="btn-outline" href="/dashboard">Dashboard</a>
