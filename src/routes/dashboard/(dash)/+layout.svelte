266bc6ee6cde72b5470294a53b8e6499e91618f8
Lachie <lachiejeffress@gmail.com>
2024-04-25 21:23:10 +1000

    .

--- src/routes/dashboard/(dash)/+layout.svelte
+++ src/routes/dashboard/(dash)/+layout.svelte
@@ -2,56 +2,66 @@
 	import Icon from '@iconify/svelte';
 	import { page } from '$app/stores';
 	import type { LayoutData } from './$types';
+	import Meta from '$lib/components/utils/Meta.svelte';
+	import SettingHead from '$lib/components/dashboard/SettingHead.svelte';
 
 	export let data: LayoutData;
 	let sidebarItems = [
 		{
-			name: 'Dashboard',
-			icon: 'mingcute:home-2-fill',
-			link: '/dashboard/welcome'
-		},
-		{
-			name: 'Restaurant',
+			name: 'Restaurant Info',
+			description: "Update your restaurant's details here.",
+			type: 'top',
 			icon: 'mingcute:shop-fill',
 			link: '/dashboard/restaurant'
 		},
 		{
-			name: 'Menu',
+			name: 'Your Menu',
+			description: 'Make changes to your menu here.',
+			type: 'top',
 			icon: 'mingcute:book-6-fill',
 			link: '/dashboard/menu'
 		},
 		{
-			name: 'Gallery',
+			name: 'Your Gallery',
+			description: 'Upload, edit and delete images you use all over your site!',
+			type: 'top',
 			icon: 'mingcute:photo-album-fill',
 			link: '/dashboard/gallery'
 		},
 		{
 			name: 'Billing',
+			description: 'Your billing',
+			type: 'top',
 			icon: 'mingcute:bank-card-fill',
 			link: '/dashboard/billing'
-		}
-	];
-
-	let accountItems = [
+		},
 		{
 			name: 'Profile',
+			description: 'Your profile',
+			type: 'bottom',
 			icon: 'mingcute:user-3-fill',
 			link: '/dashboard/profile'
 		},
 		{
 			name: 'Support',
+			description: "Need help with your restaurant? Running into issues? We're here to help.",
+			type: 'bottom',
 			icon: 'mingcute:question-fill',
 			link: '/dashboard/support'
 		},
 		{
 			name: 'Logout',
+			description: 'Logout',
+			type: 'bottom',
 			icon: 'mingcute:exit-line',
 			link: '/dashboard/logout'
 		}
 	];
 
-	$: currentRoute = $page.url.pathname;
+	$: currentRoute = sidebarItems.find((item) => item.link === $page.url.pathname);
 </script>
+
+<Meta title="Dashboard" description="Your dashboard" />
 
 <div class=" flex min-h-screen w-screen">
 	<div
@@ -62,36 +72,46 @@
 				F3AST
 			</div>
 			{#each sidebarItems as item}
-				<a
-					data-sveltekit-preload-data
-					href={item.link}
-					title={item.name}
-					class="group flex h-16 items-center justify-center text-2xl hover:bg-neutral-100"
-				>
-					<Icon
-						icon={item.icon}
-						class={`${currentRoute === item.link ? 'scale-[120%] text-emerald-300' : ''} transition-all group-hover:scale-[125%]`}
-					/>
-				</a>
+				{#if item.type === 'top'}
+					<a
+						data-sveltekit-preload-data
+						href={item.link}
+						title={item.name}
+						class="group flex h-16 items-center justify-center text-2xl hover:bg-neutral-100"
+					>
+						<Icon
+							icon={item.icon}
+							class={`${currentRoute?.link === item.link ? 'scale-[120%] text-emerald-300' : ''} transition-all group-hover:scale-[125%]`}
+						/>
+					</a>
+				{/if}
 			{/each}
 		</div>
 		<div class="flex flex-col divide-y divide-neutral-400 border-t border-neutral-400">
-			{#each accountItems as item}
-				<a
-					data-sveltekit-preload-data
-					href={item.link}
-					title={item.name}
-					class="group flex h-16 items-center justify-center text-2xl hover:bg-neutral-100"
-				>
-					<Icon
-						icon={item.icon}
-						class={`${currentRoute === item.link ? 'scale-[120%] text-emerald-300' : ''} transition-all group-hover:scale-[125%]`}
-					/>
-				</a>
+			{#each sidebarItems as item}
+				{#if item.type === 'bottom'}
+					<a
+						data-sveltekit-preload-data
+						href={item.link}
+						title={item.name}
+						class="group flex h-16 items-center justify-center text-2xl hover:bg-neutral-100"
+					>
+						<Icon
+							icon={item.icon}
+							class={`${currentRoute?.link === item.link ? 'scale-[120%] text-emerald-300' : ''} transition-all group-hover:scale-[125%]`}
+						/>
+					</a>
+				{/if}
 			{/each}
 		</div>
 	</div>
 	<div class="ml-16 flex-grow overflow-y-auto bg-neutral-100 p-2 sm:p-8 md:p-16">
-		<slot />
+		<div class="mx-auto flex w-full max-w-3xl flex-col gap-4">
+			<Meta
+				title={currentRoute?.name + ' | Dashboard'}
+				description={currentRoute?.description || ''}
+			/>
+			<slot />
+		</div>
 	</div>
 </div>
