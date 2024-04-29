27595e78d477a6c3a455d234ef25902975afded9
Lachie <lachiejeffress@gmail.com>
2024-04-29 19:34:21 +1000

    .

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -1,47 +1,55 @@
-import { json } from '@sveltejs/kit';
-import { MAIL_USER, MAIL_PASS } from '$env/static/private';
-import nodemailer from 'nodemailer';
+import { BREVO_API_KEY } from '$env/static/private';
 
 export const sendEmail = async (
 	toEmail: string,
+	toName: string | null,
 	fromEmail: string,
+	fromName: string,
 	subject: string,
 	html: string | null,
 	text: string | null
 ) => {
+	// Validate input parameters
+	if (!toEmail || !fromEmail || !subject) {
+		throw new Error('Missing required email parameters.');
+	}
 	if (!html && !text) {
 		throw new Error('No message content provided.');
 	}
 
-	const transporter = nodemailer.createTransport({
-		host: 'smtp-relay.brevo.com',
-		port: 587,
-		secure: false,
-		auth: {
-			user: MAIL_USER,
-			pass: MAIL_PASS
-		}
-	});
-
-	const emailOptions = {
-		from: fromEmail,
-		to: toEmail,
-		subject: subject,
-		...(html ? { html } : { text: text || '' })
-	};
-
-	transporter
-		.sendMail(emailOptions)
-		.then(() => {
-			return json({
-				success: true,
-				message: 'Email sent successfully.'
-			});
-		})
-		.catch((error) => {
-			return json({
-				success: false,
-				message: `Error sending email: ${error.message}`
-			});
+	try {
+		const response = await fetch('https://api.brevo.com/v3/smtp/email', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'api-key': BREVO_API_KEY
+			},
+			body: JSON.stringify({
+				sender: {
+					name: fromName,
+					email: fromEmail
+				},
+				to: [
+					{
+						email: toEmail,
+						name: toName
+					}
+				],
+				...(html ? { htmlContent: html } : { textContent: text || '' }),
+				subject
+			})
 		});
+
+		// Check if the request was successful
+		if (!response.ok) {
+			const errorData = await response.json();
+
+			console.log(errorData);
+
+			throw new Error(`Failed to send email: ${errorData.message}`);
+		}
+	} catch (error) {
+		console.error('Error sending email:', error);
+		throw new Error('Email sending failed due to an unexpected error.');
+	}
 };
