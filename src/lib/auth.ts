22fa91d67a8b66c3b53bec3d3cea1af25ed1a9ac
StanLachie <lachiejeffress@gmail.com>
2024-12-30 21:21:18 +1100

    Update Prisma configuration: Change DATABASE_URL to VITE_DATABASE_URL for compatibility with Vite, and refactor Prisma client initialization in auth and prisma files for improved structure and performance.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,8 @@
 import { PUBLIC_BASE_URL } from "$env/static/public";
-import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
+import prisma from "./prisma";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
   basePath: "/api/auth",
   baseURL: PUBLIC_BASE_URL,
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,27 +4,11 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import ws from "ws";
 
-declare global {
-  var __prisma: PrismaClient | undefined;
-}
-// Configure Neon
 neonConfig.webSocketConstructor = ws;
-
-neonConfig.poolQueryViaFetch = true;
-
-const connectionString = import.meta.env.DATABASE_URL;
+const connectionString = import.meta.env.VITE_DATABASE_URL;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
-
-const prisma =
-  globalThis.__prisma ||
-  new PrismaClient({
-    adapter,
-  });
-
-if (import.meta.env.DEV) {
-  globalThis.__prisma = prisma;
-}
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
