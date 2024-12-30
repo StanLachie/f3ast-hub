b204dc5a7b76a5be81c03bf11989bcd63d5c0835
StanLachie <lachiejeffress@gmail.com>
2024-12-31 00:34:53 +1100

    Refactor Prisma configuration: Update database connection URLs in schema.prisma and prisma.ts to use DATABASE_URL and DATABASE_URL_UNPOOLED for improved compatibility and flexibility in database connections.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
 import { Pool, neonConfig } from "@neondatabase/serverless";
 
-import { PrismaClient } from "@prisma/client/edge";
+import { DATABASE_URL } from "$env/static/private";
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import ws from "ws";
 
 export const runtime = "edge";
 neonConfig.webSocketConstructor = ws;
-const connectionString = import.meta.env.VITE_DATABASE_URL;
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString: DATABASE_URL });
 const adapter = new PrismaNeon(pool);
 const prisma = new PrismaClient({ adapter });
 
